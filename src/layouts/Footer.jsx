e7c3aa387276b25574cc4ed46d7937bb83619514
Aku-12 <akashchaudhary205698@gmail.com>
2025-06-12 22:26:13 +0545

    implemeted header and footer

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer = () => {
+    return (
+        <footer className="text-center p-4"><p>2025 MyApp. All rights reserved</p></footer>
+    )
+}
+export default Footer
---
+++ src/layouts/Header.jsx
@@ -0,0 +1,64 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="bg-amber-50 p-4">
+      <div className="container mx-auto flex flex-row justify-between items-center">
+        {/* Logo */}
+        <div className="text-lg font-bold">Thrill Quest</div>
+
+        {/* Navigation Links */}
+        <div className="flex flex-row space-x-8">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "text-indigo-600 font-semibold" : "text-gray-800"
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              isActive ? "text-red-500 font-semibold" : "text-gray-800"
+            }
+          >
+            Explore
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 font-semibold" : "text-gray-800"
+            }
+          >
+            Contact
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              isActive ? "text-blue-500 font-semibold" : "text-gray-800"
+            }
+          >
+            About Us
+          </NavLink>
+        </div>
+
+        {/* Auth Buttons */}
+        <div className="flex space-x-8 px-5">
+          <NavLink to="/login">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-xl">
+              Login
+            </button>
+          </NavLink>
+          <NavLink to="/register">
+            <button className="bg-green-500 text-white px-4 py-2 rounded-xl">
+              Signup
+            </button>
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
