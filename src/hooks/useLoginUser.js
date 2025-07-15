b5a5b84047eaa0b286c66094057111050b062aac
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-15 16:38:28 +0545

    login and register hooks are implemented

--- src/hooks/useLoginUser.js
+++ src/hooks/useLoginUser.js
@@ -1,12 +1,12 @@
 import { useMutation } from "@tanstack/react-query"
-import { loginService } from "../services/AuthService"
+import { loginService } from "../services/authService"
 import { toast } from "react-toastify"
 import { useContext } from "react"
 import { AuthContext } from "../auth/AuthProvider"
 import roleRoutes from "../../utils/roleRoutes"
 import { useNavigate } from "react-router-dom"
 
-const useLoginUser=()=>{
+const useLoginUser=(onSuccessCallback)=>{
     const {login} = useContext(AuthContext)
     const navigate = useNavigate();
     return useMutation(
@@ -19,6 +19,7 @@ const useLoginUser=()=>{
                 login(data?.data,data?.token)
                 const route = roleRoutes[data?.data?.role]
                 navigate(route)
+                if (onSuccessCallback) onSuccessCallback();
             },
             onError:(err)=>{
                 toast.error(err.message || "Login Failed")
--- src/hooks/useRegisterUser.js
+++ src/hooks/useRegisterUser.js
@@ -1,14 +1,15 @@
 import { useMutation } from "@tanstack/react-query"
-import {registerService } from "../services/AuthService"
+import {registerService } from "../services/authService"
 import { toast } from "react-toastify"
 
-const useRegisterUser=()=>{
+const useRegisterUser=(onSuccessCallback)=>{
     return useMutation(
         {
             mutationFn:registerService,
             mutationKey:['register-key'],
             onSuccess:(data)=>{
                 toast.success(data.message || "Register Successfull")
+                if (onSuccessCallback) onSuccessCallback();
             },
             onError:(err)=>{
                 toast.error(err.message || "Register Failed")
