b89c0db45f275fe9f750e1216edeec1eaa0e4841
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-15 23:47:10 +0545

    explore page added

---
+++ src/pages/home/ExplorePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ExploreSection from '../../components/user/components/ExploreSection'
+
+function ExplorePage() {
+  return (
+    <div>
+     <ExploreSection/> 
+    </div>
+  )
+}
+
+export default ExplorePage
